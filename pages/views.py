06c7d8e0b71fb9cda45f1390fb5c5caca7627da6
almostdevelopr <minhalam.dev@gmail.com>
2022-09-09 17:56:45 +0530

    Add about page dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from listings.models import Listing
+from teams.models import Team
 
 
 def index(request):
@@ -14,4 +15,15 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all teammates
+    team_realtors=Team.objects.order_by('-hire_date')
+
+    # get mvp
+    mvp_team_realtors=Team.objects.all().filter(is_mvp=True)
+
+    context={
+        'team_realtors':team_realtors,
+        'mvp_team_realtors':mvp_team_realtors
+    }
+
+    return render(request, 'pages/about.html',context)
