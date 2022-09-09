675fc4617e80b67059b19f37a78466d84089cc90
almostdevelopr <minhalam.dev@gmail.com>
2022-09-09 15:28:24 +0530

    Add custom search,list per page

--- listings/admin.py
+++ listings/admin.py
@@ -6,6 +6,13 @@ from .models import Listing
 class ListingAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_published',
                     'price', 'list_date', 'team_realtor')
+    list_display_links = ('id', 'title')
+    # filter box in team_realtor
+    list_filter = ('team_realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city',
+                     'state', 'zipcode', 'price')  # search in listings
+    list_per_page = 25
 
 
 admin.site.register(Listing, ListingAdmin)
