3fe2a79963dced5e60cba63d24bf37ac4c47719f
almostdevelopr <minhalam.dev@gmail.com>
2022-09-09 15:22:09 +0530

    Add listings table in admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import Listing
-# Register your models here.
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'team_realtor')
+
+
+admin.site.register(Listing, ListingAdmin)
