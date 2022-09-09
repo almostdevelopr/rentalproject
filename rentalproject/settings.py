36123ed3aa2004b0eee26735292788a4fafd1a40
almostdevelopr <minhalam.dev@gmail.com>
2022-09-09 14:53:21 +0530

    Add media urls

--- rentalproject/settings.py
+++ rentalproject/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rentalprojectdb',
         'USER': 'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'localhost'
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
@@ -122,11 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'rentalproject/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'rentalproject/static')
 ]
+
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
