1f82ede2a26f07436914e5d0547f5034531b8010
almostdevelopr <minhalam.dev@gmail.com>
2022-09-09 15:32:33 +0530

    Add custom teams panel

--- teams/admin.py
+++ teams/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from .models import Team
-# Register your models here.
-admin.site.register(Team)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Team, TeamAdmin)
