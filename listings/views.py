0203b54d8d01e0450702c8917df20027c264e23b
almostdevelopr <minhalam.dev@gmail.com>
2022-09-10 12:39:02 +0530

    Add single listing page

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
@@ -20,7 +20,16 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    # use listing model
+    
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+    context={
+        'listing':listing
+    }
+
+
+    return render(request, 'listings/listing.html',context)
 
 
 def search(request):
