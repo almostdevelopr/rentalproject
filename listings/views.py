ebd733899a90d09af57fc20758007f224f4b5c5a
almostdevelopr <minhalam.dev@gmail.com>
2022-09-10 14:00:20 +0530

    Add search form choices

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404,render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 # Create your views here.
@@ -33,4 +33,11 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context={
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'state_choices':state_choices
+    }
+
+    return render(request, 'listings/search.html',context)
