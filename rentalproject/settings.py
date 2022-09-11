64b19821a3e2434c181de84976e7dcf06216631f
almostdevelopr <minhalam.dev@gmail.com>
2022-09-11 11:34:36 +0530

    Add accounts app and url setup

--- rentalproject/settings.py
+++ rentalproject/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'teams.apps.TeamsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
