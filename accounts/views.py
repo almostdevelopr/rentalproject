4ea5aef38d4a8e4d28c79ca7298429b6c8623a40
almostdevelopr <minhalam.dev@gmail.com>
2022-09-11 12:46:24 +0530

    Add user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,68 @@
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check if passwords match
+        if password == confirm_password:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # Register user
+                    user = User.objects.create_user(
+                        username=username,
+                        password=password,
+                        email=email,
+                        first_name=first_name,
+                        last_name=last_name
+                    )
+
+                    # Method 1 :
+                    # Login after registration
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+
+                    # Method 2
+                    # Register the user and redirect to login page
+                    user.save()
+                    messages.success(
+                        request, 'You are now registered amd can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
-        return render(request,'accounts/register.html')
+        return render(request, 'accounts/register.html')
+
 
 def login(request):
     if request.method == 'POST':
         # Login user
         return
     else:
-        return render(request,'accounts/login.html')
+        return render(request, 'accounts/login.html')
+
 
 def logout(request):
     return redirect('index')
 
+
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html')
