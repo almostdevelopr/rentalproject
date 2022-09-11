a07dee562315764605fd589f87d28744b0a5e18b
almostdevelopr <minhalam.dev@gmail.com>
2022-09-11 13:33:21 +0530

    Add contacts app and model

--- rentalproject/settings.py
+++ rentalproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'teams.apps.TeamsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
